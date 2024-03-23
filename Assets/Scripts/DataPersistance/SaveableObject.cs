91d29cb27553b819cf0b2d463b23f7eca7afcf95
Marco199609 <marco19-96@hotmail.com>
2024-03-23 20:49:53 -0300

    Added improved GUID generator

--- Assets/Scripts/DataPersistance/SaveableObject.cs
+++ Assets/Scripts/DataPersistance/SaveableObject.cs
@@ -1,15 +1,39 @@
+using System;
+using UnityEditor;
 using UnityEngine;
 
 [ExecuteInEditMode]
 public class SaveableObject : MonoBehaviour
 {
     [field: SerializeField] public string Id { get; private set; }
+
+    private float delayInSeconds = 30;
+
     private void OnEnable()
     {
-        if (string.IsNullOrEmpty(Id))
+        EditorApplication.update += GenerateGUID;
+    }
+
+    private void GenerateGUID()
+    {
+        if(EditorApplication.timeSinceStartup < delayInSeconds && (string.IsNullOrEmpty(Id)))
         {
-            Id = System.Guid.NewGuid().ToString();
-            Debug.Log($"Generated GUID for {gameObject}.");
+            int timeLeft = (int)delayInSeconds - (int)EditorApplication.timeSinceStartup;
+            Debug.Log($"Will attempt {gameObject.name} GUID update in: {timeLeft}.");
+        }
+        else
+        {
+            if (string.IsNullOrEmpty(Id))
+            {
+                Id = Guid.NewGuid().ToString();
+                Debug.Log($"Generated new GUID for {gameObject.name}. If this object existed previously, save file deletion is recommended.");
+            }
+            else
+            {
+                Debug.Log($"Detected existing GUID for {gameObject.name}.");
+            }
+
+            EditorApplication.update -= GenerateGUID;
         }
     }
 }
